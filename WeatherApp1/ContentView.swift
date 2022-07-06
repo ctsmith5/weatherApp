7912a317019bb253f089123f146d6af777ffae5d
Colin Smith <ctsmith5@ncsu.edu>
2022-07-06 11:56:16 -0400

    Initial Commit

---
+++ WeatherApp1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherApp1
+//
+//  Created by Colin Smith on 7/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherApp1/WeatherApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp1App.swift
+//  WeatherApp1
+//
+//  Created by Colin Smith on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
